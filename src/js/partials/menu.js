331638203f45c979f42c92991798287a4120a9b0
Mikhail Tukai <tukaym@gmail.com>
2016-09-28 13:47:50 +0300

    added main menu

--- src/js/partials/menu.js
+++ src/js/partials/menu.js
@@ -1,7 +1,14 @@
 var button = document.querySelector('.menu-button');
 var menuButton = document.querySelector('.sandwich');
+var menu = document.querySelector('.main-menu');
+var links = menu.querySelectorAll('li a');
 function menuToggle() {
   menuButton.classList.toggle('active');
+  menu.classList.toggle('is-active');
 }
 
 button.addEventListener('click', menuToggle);
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', menuToggle);
+}
