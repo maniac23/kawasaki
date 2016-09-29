97676e9ea15eae53c75e77c93bad557764a6be33
Mikhail Tukai <tukaym@gmail.com>
2016-09-29 18:16:45 +0300

    added pagescroll to id

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
  */
  //= ../../bower_components/jquery/dist/jquery.js
  //= ../../bower_components/slick-carousel/slick/slick.js
+ //= ../../bower_components/page-scroll-to-id/jquery.malihu.PageScroll2id.js
 
 
 $(document).ready(function() {
@@ -10,4 +11,5 @@ $(document).ready(function() {
   //= partials/slider.js
   //= partials/quotes_slider.js
   //= partials/video.js
+  $('a[href*=\'#\']').mPageScroll2id();
 });
