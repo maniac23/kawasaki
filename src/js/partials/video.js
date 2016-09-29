a27046c3d414dde07afdd8d7ebe8894285f180df
Mikhail Tukai <tukaym@gmail.com>
2016-09-29 16:30:54 +0300

    added video block for desktops

--- src/js/partials/video.js
+++ src/js/partials/video.js
@@ -1,10 +1,13 @@
 var videoBtn = document.querySelector('.video__btn');
 var video = document.querySelector('.video__bg iframe');
+var text = document.querySelector('.video__title');
 
 function showVideo() {
   video.classList.remove('hidden');
-  videoBtn.classList.add('hidden');
   video.src = 'https://www.youtube.com/embed/Mtjatz9r-Vc?autoplay=1';
+  videoBtn.classList.add('hidden');
+  text.classList.add('hidden');
+
 }
 
 videoBtn.addEventListener('click', showVideo);
