ee07522762bb29cc7f2305ac5595820f8a375801
Mikhail Tukai <tukaym@gmail.com>
2016-09-30 11:00:56 +0300

    added separated menus for mobile & desktop

--- src/js/partials/menu.js
+++ src/js/partials/menu.js
@@ -1,12 +1,12 @@
 var button = $('.menu-button');
 var menuButton = $('.sandwich');
-var menu = $('.main-menu');
+var menu = $('.mobile-menu');
 
-$('.menu-button, .main-menu li a').click(function() {
+$('.menu-button, .mobile-menu li a').click(function() {
   menuButton.toggleClass('active');
   if(menu.is(':visible')) {
     menu.fadeOut(600);
   } else {
     menu.fadeIn(600).css('display', 'flex');
-	                                        }
+  }
 });
